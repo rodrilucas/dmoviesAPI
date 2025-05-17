d29def37e243c276812203eb00b915c6fe20dd5f
rodrilucas <lucas.celery@gmail.com>
2025-05-17 10:18:38 -0300

    feat: create functions that validate the body, parameter and query of requests

---
+++ src/middlewares/validateSchema.js
@@ -0,0 +1,32 @@
+export function validateQuery(schema) {
+  return (req, res, next) => {
+    try {
+      req.query = schema.parse(req.query);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+export function validateParams(schema) {
+  return (req, res, next) => {
+    try {
+      req.params = schema.parse(req.params);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+export function validateBody(schema) {
+  return (req, res, next) => {
+    try {
+      req.body = schema.parse(req.body);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
