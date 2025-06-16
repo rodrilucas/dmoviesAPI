6f9be0ff5d8216ae2f29d87d567270c62b19a441
rodrilucas <lucas.celery@gmail.com>
2025-06-16 18:46:02 -0300

    fix: database url

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,7 @@ export const config = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
+      connectionString: process.env.DATABASE_URL,
     },
     pool: { min: 0, max: 10 },
     migrations: {
